8ac11826f03bf10acfc31e62d7a592b3adcc8ed0
Tobeyw <tobey1024@126.com>
2025-02-17 18:57:59 +0800

    update event name for bridge

--- neo3fura_http/biz/api/bridge.GetBridgeTxByNonce.go
+++ neo3fura_http/biz/api/bridge.GetBridgeTxByNonce.go
@@ -28,7 +28,7 @@ func (me *T) GetBridgeTxByNonce(args struct {
 	}
 
 	filter = bson.M{"contract": args.ContractHash.Val(),
-		"$or":                 []interface{}{bson.M{"eventname": "GasWithdrawal"}, bson.M{"eventname": "GasClaimable"}},
+		"$or":                 []interface{}{bson.M{"eventname": "GasWithdrawal"}, bson.M{"eventname": "NativeClaimable"}},
 		"state.value.0.value": nonceStr,
 	}
 
